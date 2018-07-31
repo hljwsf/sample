1ecbfac5e4229088bb13984d2c4f6b33f226e5b2
vagrant <vagrant@homestead>
2018-07-31 08:59:12 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
 class UsersController extends Controller
@@ -31,6 +32,7 @@ class UsersController extends Controller
 			'password' => bcrypt($request->password),
 		]);
 
+		Auth::login($user);
 		session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
 		return redirect()->route('users.show',[$user]);
 	}
